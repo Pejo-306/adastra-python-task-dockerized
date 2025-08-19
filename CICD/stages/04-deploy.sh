f6b59ca6d1c7d46bdcaedb1330317358c5428e43
Petar Nikolov <penikolov23@gmail.com>
2025-08-19 17:34:10 +0300

    [2025] Add production engine's IP to known SSH hosts

--- CICD/stages/04-deploy.sh
+++ CICD/stages/04-deploy.sh
@@ -1,1 +1,2 @@
-docker -H "ssh://$PROD_ENGINE?sshOptions=-o StrictHostKeyChecking=no" stack deploy -c docker-compose.prod-swarm.yml aptd-prod
+ssh-keyscan -H $(echo $PROD_ENGINE | cut -d@ -f2) >> ~/.ssh/known_hosts
+docker -H "ssh://$PROD_ENGINE" stack deploy -c docker-compose.prod-swarm.yml aptd-prod
