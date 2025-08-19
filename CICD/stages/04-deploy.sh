81651b3f1e0851b0a52017a9cffe89c69b9c4896
Petar Nikolov <penikolov23@gmail.com>
2025-08-19 16:51:26 +0300

    [2025] Remove version from generated Docker Swarm compose config

--- CICD/stages/04-deploy.sh
+++ CICD/stages/04-deploy.sh
@@ -1,2 +1,5 @@
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml config | sed -E "s/cpus: ([0-9\\.]+)/cpus: '\\1'/"  > docker-compose.prod-swarm.yml
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml config \
+  | sed -E "s/cpus: ([0-9\\.]+)/cpus: '\\1'/" \
+  | sed '/^version:/d' \
+  > docker-compose.prod-swarm.yml
 docker -H "ssh://$PROD_ENGINE" stack deploy -c docker-compose.prod-swarm.yml aptd-prod
