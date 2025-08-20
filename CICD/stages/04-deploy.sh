323fc518169c87b2fe4b0068342656d85f152f97
Petar Nikolov <penikolov23@gmail.com>
2025-08-20 19:53:41 +0300

    [2025] Add super verbose mode so we can debug

--- CICD/stages/04-deploy.sh
+++ CICD/stages/04-deploy.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+set -x  # optional: bash debugging to echo each command
+
 export DOCKER_HOST="ssh://$PROD_ENGINE"
-export DOCKER_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+export DOCKER_SSH_COMMAND="ssh -vvv -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
 docker stack deploy -c docker-compose.prod-swarm.yml aptd-prod
+
