79482e690ba213307752981fb2d9cd6f675725c9
Petar Nikolov <penikolov23@gmail.com>
2025-08-20 20:39:31 +0300

    [2025] Switch to using ED25519 SSH key

--- CICD/stages/04-deploy.sh
+++ CICD/stages/04-deploy.sh
@@ -3,14 +3,16 @@ set -euo pipefail
 set -x  # optional: echo each command for debug
 
 # Where to deploy
+KEY="/root/.ssh/id_ed25519"             # private key inside Jenkins container
 REMOTE=$PROD_ENGINE                     # e.g. ip172-18-0-65-xxxx@direct.labs.play-with-docker.com
 STACK_NAME="aptd-prod"
 REMOTE_PATH="/tmp/docker-compose.yml"
+SSH_OPTS="-i $KEY -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
 # 1) Copy the file over
 echo "Uploading stack file..."
-scp -o StrictHostKeyChecking=no docker-compose.prod-swarm.yml "${REMOTE}:${REMOTE_PATH}"
+scp $SSH_OPTS docker-compose.prod-swarm.yml "${REMOTE}:${REMOTE_PATH}"
 
 # 2) Run the deploy command over SSH
 echo "Deploying via SSH..."
-ssh -o StrictHostKeyChecking=no "$REMOTE" "docker stack deploy -c ${REMOTE_PATH} ${STACK_NAME}"
+ssh $SSH_OPTS "$REMOTE" "docker stack deploy -c ${REMOTE_PATH} ${STACK_NAME} --with-registry-auth && docker stack services $STACK_NAME"
