da22715eae6de69aaed307db06cb8e240765e136
Petar Nikolov <penikolov23@gmail.com>
2025-08-20 19:18:24 +0300

    [2025] Simplify deployment command

--- CICD/stages/04-deploy.sh
+++ CICD/stages/04-deploy.sh
@@ -1,3 +1,1 @@
-export DOCKER_HOST="ssh://$PROD_ENGINE"
-export DOCKER_SSH_CMD="ssh -o StrictHostKeyChecking=no"
-docker stack deploy -c docker-compose.prod-swarm.yml aptd-prod
+docker -H "ssh://$PROD_ENGINE" stack deploy -c docker-compose.prod-swarm.yml aptd-prod
